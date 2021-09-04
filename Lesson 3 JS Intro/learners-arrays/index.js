20cc6f2025cca3e424d163b08405f7870638ffe6
Jane Lim <86217813+janelsj@users.noreply.github.com>
2021-09-04 15:38:17 +0800

    Delete Lesson 3 JS Intro directory

--- Lesson 3 JS Intro/learners-arrays/index.js
+++
@@ -1,8 +0,0 @@
-/*
-    Use instructor's code sample as reference to try out the following:
-        - Declare and initialize array.
-        - Push and pop array.
-        - Working with index
-        - Check if an element exists in array
-
-*/
