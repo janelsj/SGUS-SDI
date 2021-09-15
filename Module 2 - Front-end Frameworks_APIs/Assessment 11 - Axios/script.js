d08b169ead9ae5d53bfc73694ad13cf472c39f62
Jane Lim <jane_limsj@hotmail.com>
2021-09-15 14:11:51 +0800

    update

---
+++ Module 2 - Front-end Frameworks_APIs/Assessment 11 - Axios/script.js
@@ -0,0 +1,17 @@
+const formEl = document.getElementById('taskInput')
+const buttonElAdd = document.querySelector("button[type='submit']")
+const buttonElClear = document.querySelector("button[type='reset']")
+const newItemEl = document.getElementById('newItem')
+const dueDateEl = document.getElementById('dueDate')
+
+document.body.addEventListener("load", string)
+var string = alert("What is your name?")
+
+const user = document.body.createElement("h3")
+user.innerText = document.write(string);
+
+function addNewItem() {
+
+}
+
+localStorage
