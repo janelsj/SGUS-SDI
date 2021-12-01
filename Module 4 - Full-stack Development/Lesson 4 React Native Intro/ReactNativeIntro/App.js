1daf7e020a2e860e4a14b16f6bb84517b81bf918
Jane Lim <jane_limsj@hotmail.com>
2021-12-01 17:31:09 +0800

    Lesson 4

---
+++ Module 4 - Full-stack Development/Lesson 4 React Native Intro/ReactNativeIntro/App.js
@@ -0,0 +1,33 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>
+      <TouchableOpacity style={styles.button} onPress={test}><Text>Tap Me</Text></TouchableOpacity>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+function test123(){
+  console.log("this is log message");
+  console.info("this is info message");
+  console.error("this is error message");
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  button: {
+    alignItems: "center",
+    backgroundColor: "#DDDDDD",
+    padding: 10
+  },
+});
