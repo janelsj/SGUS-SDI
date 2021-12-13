2284b8fe07793a7c682d91ecb5f6bf823b270171
Jane Lim <jane_limsj@hotmail.com>
2021-12-13 09:40:05 +0000

    completed assignment 6

---
+++ Module 4 - Full-stack Development/Assignment 6 - React Native Navigation/DrawerApp/App.js
@@ -0,0 +1,13 @@
+import 'react-native-gesture-handler';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+
+const Drawer = createDrawerNavigator();
+
+export default function MyDrawer() {
+  return (
+    <Drawer.Navigator>
+      <Drawer.Screen name="Feed" component={Feed} />
+      <Drawer.Screen name="Article" component={Article} />
+    </Drawer.Navigator>
+  );
+}
